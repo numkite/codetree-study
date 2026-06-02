f61bb2eb0efb9217be439e54647fa87d6df70573
Sena_수연 <nosuyeon22@gmail.com>
2026-06-02 15:48:03 +0900

    [Passed] 숫자의 증감 (51ms, 16MB)

---
+++ trail1/숫자의 증감/increasing-and-decreasing-numbers.py
@@ -0,0 +1,11 @@
+up = input().split()
+c = up[0]
+n = int(up[1])
+
+if c == 'A':
+    for i in range(1, n+1):
+        print(i, end = ' ')
+        i += 1
+if c == 'D':
+    for i in range(n, 0, -1):
+        print(i, end = ' ')
